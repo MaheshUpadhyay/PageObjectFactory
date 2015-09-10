da468644e6ce359957658af0c0a37d37b233715d
Mahesh <Mahesh@192.168.0.6>
2015-09-10 20:14:53 +0530

    initial commit

---
+++ GitHub/src/gitPackage/Class1.java
@@ -0,0 +1,11 @@
+package gitPackage;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		ParentClass p = new ParentClass();
+		p.print();
+
+	}
+
+}
---
+++ GitHub/src/gitPackage/ParentClass.java
@@ -0,0 +1,10 @@
+package gitPackage;
+
+public class ParentClass {
+	public void print(){
+		System.out.println("in Parent class");
+	}
+	public void print1(){
+		System.out.println("in Parent1 class");
+	}
+}
