da1cba8118b50da5e4ac740efed0650f9b6e6591
Mahesh <Mahesh@192.168.0.6>
2015-09-10 20:45:24 +0530

    Added 1st output.

--- GitHub/src/gitPackage/ParentClass.java
+++ GitHub/src/gitPackage/ParentClass.java
@@ -3,6 +3,7 @@ package gitPackage;
 public class ParentClass {
 	public void print(){
 		System.out.println("in Parent class");
+		System.out.println("1st commit");
 	}
 	public void print1(){
 		System.out.println("in Parent1 class");
